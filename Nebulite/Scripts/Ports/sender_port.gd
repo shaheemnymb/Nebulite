6d0ff2dfeaed2ad8b96f1bcc179e047e47d56a8d
shaheemnymb <jk5526335@gmail.com>
2025-04-24 15:31:48 +0300

    Fixed Crouching
    With an ALien Gun

--- Nebulite/Scripts/Ports/sender_port.gd
+++ Nebulite/Scripts/Ports/sender_port.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+
+var button_pressed = false
+var line_2d : Line2D
+
 func get_clicked_sprite():
 	var raycast := RayCast2D.new()
 	add_child(raycast)
@@ -14,6 +18,21 @@ func get_clicked_sprite():
 	raycast.queue_free()
 
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("Left mouse clicked"):
-		get_clicked_sprite()
+func _process(_delta: float) -> void:
+	if button_pressed:
+		line_2d.clear_points()
+		line_2d.add_point(global_position)
+		line_2d.add_point(get_global_mouse_position())
+
+
+func _on_button_button_down() -> void:
+	print("Button down")
+	button_pressed = true
+	line_2d = Line2D.new()
+	add_child(line_2d)
+
+
+func _on_button_button_up() -> void:
+	print("Button up")
+	button_pressed = false
+	line_2d.queue_free()
